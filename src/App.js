475d0f20221376b41995f7c9f4ad698077ee36ba
ChaudharyAnshul <144816995+ChaudharyAnshul@users.noreply.github.com>
2024-01-01 14:01:31 -0500

    file structure renamed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import ReactGA from 'react-ga';
 import $ from "jquery";
-import { Header } from './components/Header';
-import { About } from './components/About';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-import { Resume } from './components/Resume';
-import { Portfolio } from './components/Portfolio';
+import { Header } from './pages/Header';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Footer } from './pages/Footer';
+import { Resume } from './pages/Resume';
+import { Portfolio } from './pages/Portfolio';
 
 export const App = () => {
   const [resumeData, setResumeData] = useState({});
