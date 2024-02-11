cde90fe6e5881bf45551ed24f778d4e4d111a51a
ChaudharyAnshul <144816995+ChaudharyAnshul@users.noreply.github.com>
2024-02-11 01:22:41 -0500

    About section improved

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,14 @@ import React from "react";
 import { motion } from "framer-motion";
 import Grid from '@mui/material/Grid';
 import { getIconByString } from './components/IconPicker';
+import Typography from '@mui/material/Typography';
 
 export const About = ({ data }) => {
 
   if (!data) return null;
 
-  const { name, image, bio1, bio2, address, phone, email } = data;
-  const { street, city, state, zip } = address;
+  const { image, bio1, bio2, bio3, address, phone, email, portfolio, degree } = data;
+  const { city, state } = address;
   const profilepic = "images/" + image;
 
   const fadeIn = {
@@ -28,6 +29,25 @@ export const About = ({ data }) => {
     );
   });
 
+  const skills = data.topSkills.map((skills) => {
+    const backgroundColor = "#009999";
+    const className = "bar-expand " + skills.name.toLowerCase();
+    const width = skills.level;
+
+    return (
+      <motion.li
+        key={skills.name}
+        initial="hidden"
+        animate="visible"
+        variants={fadeIn}
+        transition={{ duration: 1 }}
+      >
+        <div>{skills.name}</div>
+        <span style={{ width, backgroundColor }} className={className}></span>
+      </motion.li>
+    );
+  });
+
   return (
     <section id="about">
       <motion.div
@@ -37,8 +57,8 @@ export const About = ({ data }) => {
         transition={{ duration: 1 }}
       >
         <div className="row">
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2 }}>
-            <Grid item xs={12} md={3}>
+          <Grid container rowSpacing={1} columnSpacing={{ sx: 1, sm: 1, md: 2 }} sx={{padding: '10px'}}>
+            <Grid item xs={12} md={3} style={{ display: 'flex', justifyContent: 'center' }}>
               <img
                 className="profile-pic"
                 src={profilepic}
@@ -49,41 +69,69 @@ export const About = ({ data }) => {
               <h2>About Me</h2>
               <p>
                 {bio1}
-                <br />
+                <br /><br />
                 {bio2}
+                <br /><br />
+                {bio3}
               </p>
-              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2 }}>
-                <Grid className="contact-details" item xs={12} md={6}>
-                  <h2>Contact Details</h2>
+              <Grid container rowSpacing={1} columnSpacing={{ sx: 1, sm: 1, md: 2 }}>
+                <Grid className="contact-details" item xs={12} sm={12} md={6}>
+                  <h2>Contact</h2>
                   <p className="address">
-                    <span>{name}</span>
-                    <br />
                     <span>
-                      {street}
-                      <br />
-                      {city}, {state} {zip}
+                      <b>Degree:</b> {degree}
                     </span>
                     <br />
-                    <span>{phone}</span>
+                    <span>
+                    <b>Location:</b> {city}, {state}
+                    </span>
                     <br />
-                    <span>{email}</span>
+                    <span>
+                    <b>Phone:</b> {phone}
+                    </span>
+                    <br />
+                    <span>
+                    <b>Email:</b> {email}
+                    </span>
+                    <br />
+                    <span>
+                    {/* <b>Portfolio:</b> <a href="" target="_blank" rel="noreferrer">port.chaudharyanshul.com</a> */}
+                    </span>
                   </p>
+                  <Typography
+                    sx={{
+                      width: '100%',
+                      textAlign: 'left',
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'left',
+                      justifyContent: 'left',
+                    }}>
+                    <a href="#resume"><button style={{borderRadius:'20px', margin: '10px'}}>Resume</button></a>
+                    <a href="#contact"><button style={{borderRadius:'20px', margin: '10px'}}>Hier Me</button></a>
+                  </Typography>
                 </Grid>
-                <Grid className="socials" item xs={12} md={6}
-                  sx={{
-                    width: '100%',
-                    textAlign: 'center',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#fff',
-                    fontSize: "18px"
-                  }}
-                >
-                  <ul className="social-links">{networks}</ul>
+                <Grid item xs={12} sm={12} md={6}>
+                  <h2>Top Skills:</h2>
+                  <div className="bars">
+                    <ul className="skills">{skills}</ul>
+                  </div>
                 </Grid>
               </Grid>
+            </Grid>
+            <Grid className="socials" item xs={12} md={12}
+                sx={{
+                  width: '100%',
+                  textAlign: 'cemter',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  color: '#fff',
+                  fontSize: "18px"
+                }}
+              >
+                <ul className="social-links">{networks}</ul>
             </Grid>
           </Grid>
         </div> 
